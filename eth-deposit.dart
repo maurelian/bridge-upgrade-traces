9e66688a3b368b80b65573ca34467700ad66d4e9
Maurelian <maurelian@protonmail.ch>
2022-04-12 17:10:58 -0400

    Update eth deposit call trace without messenger

--- eth-deposit.dart
+++ eth-deposit.dart
@@ -3,18 +3,14 @@
 // call graph was simplified to remove proxies
 - depositETH in L1StandardBridge -> emits ETHDepositInitiated()
   - JUMP: _initiateETHDeposit in L1StandardBridge
-    - INLINE: code constructs calldata for CrossDomainMessenger to forward // can probably get rid of this step
-    - JUMP: LIB_CrossDomainEnabled.sendCrossDomainMessage in L1StandardBridge
-      - CALL: sendMessage in L1CrossDomainMessenger -> emits SentMessage()
-        - CALL: getQueueLength in CanonicalTransactionChain // used as nonce
-        - JUMP: Lib_CrossDomainUtils.encodeXDomainCalldata in L1CrossDomainMessenger
-        - JUMP: _sendXDomainMessage in L1CrossDomainMessenger
-          - CALL: enqueue in CanonicalTransactionChain -> emits TransactionEnqueued()
+  - JUMP: LIB_CrossDomainEnabled.sendCrossDomainMessage in L1StandardBridge -> emits SentMessage()
+    - CALL: getQueueLength in CanonicalTransactionChain // used as nonce
+    - JUMP: Lib_CrossDomainUtils._sendXDomainMessage in L1StandardBridge // moves from messenger into lib
+      - CALL: enqueue in CanonicalTransactionChain -> emits TransactionEnqueued()
 
 // Here is the L2 side of the deposit
 // https://dashboard.tenderly.co/tx/optimistic/0x9ba16a48a659cc03c27793f3d81e1bb6c09b40e31d034448797c545a9bdec00f
-- relayMessage in OVM_L2CrossDomainMessenger
-  - JUMP: require(AddressAliasHelper.undoL1ToL2Alias(msg.sender) == l1CrossDomainMessenger) // Validates the L1 caller address
-  - CALL: finalizeDeposit in OVM_L2StandardBridge -> emits DepositFinalized()
-    - CALL: mint in OVM_ETH
-      - JUMP: _mint in OVM_ETH -> emits Mint(), emits Transfer()
+finalizeDeposit in OVM_L2StandardBridge -> emits DepositFinalized()
+- JUMP: require(AddressAliasHelper.undoL1ToL2Alias(msg.sender) == l1CrossDomainMessenger) // Validates the L1 caller address
+- CALL: mint in OVM_ETH
+  - JUMP: _mint in OVM_ETH -> emits Mint(), emits Transfer()
